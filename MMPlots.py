dc7c00ab24362f36174066fb42ec4f7d8c3a6367
Lorenzo Pezzotti <34685792+lopezzot@users.noreply.github.com>
2019-01-22 10:43:05 +0100

    test commit

--- MMPlots.py
+++ MMPlots.py
@@ -8,6 +8,7 @@ import glob
 
 #path = raw_input("Insert folder to study: ")
 #
+#
 #path1 = "SM1_FROM_2018_11_16_10_00_00_TO_2018_11_16_13_51_59/"
 #path2 = "Export/"+path1+"/"
 name = raw_input("Insert filename: ")
