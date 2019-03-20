2db0bbf65c8c99aa82a11a36fd32f1526749d3ce
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2019-03-20 10:27:10 +0100

    piechart

---
+++ execute.py
@@ -0,0 +1,14 @@
+import os
+import MMPlots
+import MMPlots_attenuation
+
+todo = raw_input("MMPlots, MMPlots_attenuation or pdf? ")
+
+if todo == "pdf":
+	os.system("python produce_pdf.py")
+
+if todo == "MMPlots":
+	MMPlots.createsummaryplots()
+
+if todo == "MMPlots_attenuation":
+	MMPlots_attenuation.createsummaryplot_attenuation()
---
+++ piecart.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def create_pie(sizes, name):
+	labels = ["In spec", "Out spec"]
+	#sizes = [30,10]
+	colors = ['green','red']
+	explode = (0,0.1)
+
+	piechart = plt.pie(sizes,explode=explode,labels=labels,colors=colors,
+		autopct='%1.1f%%', shadow=True, startangle=140)
+
+	plt.axis('equal')
+	#plt.show()
+	plt.savefig(str(name), bbox_inches='tight')
+	plt.close()
+
