504faddf03065b9a0e6815a0d8aeebde088a5dc8
NataliaBM <natibm17@gmail.com>
2019-07-17 10:58:13 +0200

    Natalia commit

--- production_site.py
+++ production_site.py
@@ -2,11 +2,16 @@ def read(filename):
     f = open(filename, "r")
     hv = []
     spike = []
+    position = []
     for data in f:
-        hv.append(data[5:8])
-        spike.append(data[9:10])
-    print spike
-    print hv
+        for i in range(len(data)):
+            if data[i:i+1] == '\t':
+                position.append(i)
+            if data[i:i+1] == '\n':
+                position.append(i)
+        hv.append(data[5:position[1]])
+        spike.append(data[position[1]+1:position[2]])
+        position = []
     return hv, spike
 
 def main():
